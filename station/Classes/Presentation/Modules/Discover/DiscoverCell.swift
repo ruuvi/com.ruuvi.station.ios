53140fddebf8aafd815fc5e8649feda77aa67ff4
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-10 19:22:12 +0500

    add discover scene (without removing out of range tags)

---
+++ station/Classes/Presentation/Modules/Discover/DiscoverCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class DiscoverCell: UITableViewCell {
+    @IBOutlet weak var identifierLabel: UILabel!
+    @IBOutlet weak var rssiImageView: UIImageView!
+}
--- station/Classes/Presentation/Modules/Welcome/WelcomeViewController.swift
+++ station/Classes/Presentation/Modules/Welcome/WelcomeViewController.swift
@@ -4,6 +4,11 @@ class WelcomeViewController: UIViewController {
     
     @IBAction func scanButtonTouchUpInside(_ sender: Any) {
         UserDefaults.standard.set(true, forKey: "hasShownWelcome")
+        
+        let discoverStoryboard = UIStoryboard(name: "Discover", bundle: .main)
+        if let discover = discoverStoryboard.instantiateInitialViewController() {
+            navigationController?.pushViewController(discover, animated: true)
+        }
     }
     
 }
