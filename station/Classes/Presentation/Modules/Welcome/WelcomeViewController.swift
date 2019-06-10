1fa7060943095af3aed8ff2b26dff7aaf6740007
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-10 19:28:48 +0500

    refactor methods

--- station/Classes/Presentation/Modules/Welcome/WelcomeViewController.swift
+++ station/Classes/Presentation/Modules/Welcome/WelcomeViewController.swift
@@ -3,12 +3,18 @@ import UIKit
 class WelcomeViewController: UIViewController {
     
     @IBAction func scanButtonTouchUpInside(_ sender: Any) {
+        rememberWelcomeWasShown()
+        openDiscover()
+    }
+    
+    private func rememberWelcomeWasShown() {
         UserDefaults.standard.set(true, forKey: "hasShownWelcome")
-        
+    }
+    
+    private func openDiscover() {
         let discoverStoryboard = UIStoryboard(name: "Discover", bundle: .main)
         if let discover = discoverStoryboard.instantiateInitialViewController() {
             navigationController?.pushViewController(discover, animated: true)
         }
     }
-    
 }
