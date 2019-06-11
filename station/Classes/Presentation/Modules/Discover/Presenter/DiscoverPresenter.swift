a2b5a1e600958fe3f2c9a1335d0a1301911c47c9
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-11 13:33:54 +0500

    pass view will appear/disappear calls to presenter

--- station/Classes/Presentation/Modules/Discover/Presenter/DiscoverPresenter.swift
+++ station/Classes/Presentation/Modules/Discover/Presenter/DiscoverPresenter.swift
@@ -4,6 +4,13 @@ class DiscoverPresenter: DiscoverModuleInput {
     weak var view: DiscoverViewInput!
 }
 
+// MARK: - DiscoverViewOutput
 extension DiscoverPresenter: DiscoverViewOutput {
+    func viewWillAppear() {
+        
+    }
     
+    func viewWillDisappear() {
+        
+    }
 }
