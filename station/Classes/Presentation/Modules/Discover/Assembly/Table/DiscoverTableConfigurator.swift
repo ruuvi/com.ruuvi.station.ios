17ceabe560ec0068599f3daadc622fc0796fee23
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-13 18:07:09 +0500

    inject router

--- station/Classes/Presentation/Modules/Discover/Assembly/Table/DiscoverTableConfigurator.swift
+++ station/Classes/Presentation/Modules/Discover/Assembly/Table/DiscoverTableConfigurator.swift
@@ -2,8 +2,12 @@ import Foundation
 
 class DiscoverTableConfigurator {
     func configure(view: DiscoverTableViewController) {
+        let router = DiscoverRouter()
+        router.transitionHandler = view
+        
         let presenter = DiscoverPresenter()
         presenter.view = view
+        presenter.router = router
         
         view.output = presenter
     }
