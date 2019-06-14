200dbaeb47c4725b51e529108c0ea66e126ab279
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-14 21:57:48 +0500

    add Menu Router

---
+++ station/Classes/Presentation/Modules/Menu/Router/MenuRouter.swift
@@ -0,0 +1,9 @@
+import LightRoute
+
+class MenuRouter: MenuRouterInput {
+    weak var transitionHandler: TransitionHandler!
+    
+    func dismiss() {
+        try! transitionHandler.closeCurrentModule().perform()
+    }
+}
