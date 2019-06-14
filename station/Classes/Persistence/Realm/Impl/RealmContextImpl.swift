ba1a8bf2b7eac9d4ef882e601b108a6b92213cd5
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-14 10:14:27 +0500

    implement realm context

---
+++ station/Classes/Persistence/Realm/Impl/RealmContextImpl.swift
@@ -0,0 +1,13 @@
+import RealmSwift
+
+class RealmContextImpl: RealmContext {
+    var main: Realm = try! Realm()
+    var bg: Realm!
+    var bgWorker: Worker = Worker()
+    
+    init() {
+        bgWorker.enqueue {
+            self.bg = try! Realm()
+        }
+    }
+}
