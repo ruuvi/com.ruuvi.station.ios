ada629dd4a203470128960e3337e5c13451fcf0d
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-14 12:46:09 +0500

    register ruuvi tag persistence

---
+++ station/Classes/Persistence/Assembly/PersistenceAssembly.swift
@@ -0,0 +1,18 @@
+import Swinject
+
+class PersistenceAssembly: Assembly {
+    func assemble(container: Container) {
+        
+        container.register(RealmContext.self) { r in
+            let context = RealmContextImpl()
+            return context
+        }.inObjectScope(.container)
+        
+        container.register(RuuviTagPersistence.self) { r in
+            let persistence = RuuviTagPersistenceRealm()
+            persistence.context = r.resolve(RealmContext.self)
+            return persistence
+        }
+        
+    }
+}
