7d6649007bbf0bffdeb6eaf25390de00a4d48785
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-14 12:47:30 +0500

    inject ruuvi tag persistence into ruuvi tag presenter

--- station/Classes/Presentation/Modules/RuuviTag/Assembly/RuuviTagConfigurator.swift
+++ station/Classes/Presentation/Modules/RuuviTag/Assembly/RuuviTagConfigurator.swift
@@ -2,12 +2,15 @@ import Foundation
 
 class RuuviTagConfigurator {
     func configure(view: RuuviTagViewController) {
+        let r = AppAssembly.shared.assembler.resolver
+        
         let router = RuuviTagRouter()
         router.transitionHandler = view
         
         let presenter = RuuviTagPresenter()
         presenter.view = view
         presenter.router = router
+        presenter.ruuviTagPersistence = r.resolve(RuuviTagPersistence.self)
         
         view.output = presenter
     }
