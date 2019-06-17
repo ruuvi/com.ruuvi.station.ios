6a9fe009bf59c56661620952a9d7521b05770225
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-17 17:35:05 +0500

    close menu when opening add ruuvi tag

---
+++ station/Classes/Presentation/Modules/Menu/Presenter/MenuModuleOutput.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol MenuModuleOutput: class {
+    func menu(module: MenuModuleInput, didSelectAddRuuviTag sender: Any?)
+}
--- station/Classes/Presentation/Modules/Menu/Router/MenuRouterInput.swift
+++ station/Classes/Presentation/Modules/Menu/Router/MenuRouterInput.swift
@@ -2,5 +2,4 @@ import Foundation
 
 protocol MenuRouterInput {
     func dismiss()
-    func openDiscover()
 }
