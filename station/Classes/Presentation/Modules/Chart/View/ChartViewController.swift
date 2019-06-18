e622c88e9def4761bc528fd08c99614cad9f9795
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-18 12:24:52 +0500

    add line chart view

--- station/Classes/Presentation/Modules/Chart/View/ChartViewController.swift
+++ station/Classes/Presentation/Modules/Chart/View/ChartViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
+import Charts
 
 class ChartViewController: UIViewController {
     var output: ChartViewOutput!
+
+    @IBOutlet weak var chartView: LineChartView!
+    
 }
 
 extension ChartViewController: ChartViewInput {
