53f6dceef91e94360ec2462955b525d3360c9a43
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-18 12:49:00 +0500

    add transition for charts

--- station/Classes/Presentation/Modules/Chart/View/ChartViewController.swift
+++ station/Classes/Presentation/Modules/Chart/View/ChartViewController.swift
@@ -9,5 +9,47 @@ class ChartViewController: UIViewController {
 }
 
 extension ChartViewController: ChartViewInput {
+    func apply(theme: Theme) {
+        
+    }
     
+    func localize() {
+        
+    }
+}
+
+extension ChartViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let count = 100
+        let range: UInt32 = 30
+        
+        let now = Date().timeIntervalSince1970
+        let hourSeconds: TimeInterval = 3600
+        
+        let from = now - (Double(count) / 2) * hourSeconds
+        let to = now + (Double(count) / 2) * hourSeconds
+        
+        let values = stride(from: from, to: to, by: hourSeconds).map { (x) -> ChartDataEntry in
+            let y = arc4random_uniform(range) + 50
+            return ChartDataEntry(x: x, y: Double(y))
+        }
+        
+        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
+        set1.axisDependency = .left
+        set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
+        set1.lineWidth = 1.5
+        set1.drawCirclesEnabled = false
+        set1.drawValuesEnabled = false
+        set1.fillAlpha = 0.26
+        set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set1.drawCircleHoleEnabled = false
+        
+        let data = LineChartData(dataSet: set1)
+        data.setValueTextColor(.white)
+        data.setValueFont(.systemFont(ofSize: 9, weight: .light))
+        
+        chartView.data = data
+    }
 }
--- station/Classes/Presentation/Modules/Chart/View/ChartViewOutput.swift
+++ station/Classes/Presentation/Modules/Chart/View/ChartViewOutput.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 protocol ChartViewOutput {
-    
+    func viewDidTapOnDimmingView()
 }
