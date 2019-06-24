777423af7aefed55b03154f0a27037171d7002a3
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-24 13:01:11 +0500

    start saving data

--- station/Classes/Application/AppDelegate.swift
+++ station/Classes/Application/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let mainRouter = MainRouter.shared
             mainRouter.openDashboard()
         }
+        r.resolve(RuuviTagDaemon.self)?.startSavingBroadcasts()
         return true
     }
 
