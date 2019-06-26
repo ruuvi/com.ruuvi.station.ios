95a0207c8ea4d3516e603b533085859aeb8babcc
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-26 16:09:12 +0500

    bias random of background to not used values

--- station/Classes/Persistence/Background/UserDefaults/BackgroundPersistenceUserDefaults.swift
+++ station/Classes/Persistence/Background/UserDefaults/BackgroundPersistenceUserDefaults.swift
@@ -2,13 +2,27 @@ import UIKit
 
 class BackgroundPersistenceUserDefaults: BackgroundPersistence {
     
+    private let usedBackgroundsUDKey = "BackgroundPersistenceUserDefaults.background.usedBackgroundsUDKey"
+    private let bgMinIndex = 1
+    private let bgMaxIndex = 9
+    
+    private var usedBackgrounds: [Int] {
+        if let usedBackgrounds = UserDefaults.standard.array(forKey: usedBackgroundsUDKey) as? [Int] {
+            return usedBackgrounds
+        } else {
+            let usedBackgrounds = Array(repeating: 0, count: bgMaxIndex - bgMinIndex + 1)
+            UserDefaults.standard.set(usedBackgrounds, forKey: usedBackgroundsUDKey)
+            return usedBackgrounds
+        }
+    }
+    
     func background(for uuid: String) -> UIImage? {
         let key = "BackgroundPersistenceUserDefaults.background." + uuid
         var id = UserDefaults.standard.integer(forKey: key)
         if id > 0 {
             return UIImage(named: "bg\(id)")
         } else {
-            id = Int(arc4random_uniform(9) + 1)
+            id = biasedToNotUsedRandom()
             UserDefaults.standard.set(id, forKey: key)
             return UIImage(named: "bg\(id)")
         }
@@ -19,4 +33,17 @@ class BackgroundPersistenceUserDefaults: BackgroundPersistence {
         UserDefaults.standard.set(id, forKey: key)
     }
     
+    func biasedToNotUsedRandom() -> Int {
+        let array = usedBackgrounds
+        if let min = array.min() {
+            let indicies = array.enumerated().compactMap({ $1 == min ? $0 : nil })
+            if indicies.count == 0 {
+                return Int(arc4random_uniform(UInt32(bgMaxIndex)) + UInt32(bgMinIndex))
+            } else {
+                return indicies[0]
+            }
+        } else {
+            return Int(arc4random_uniform(UInt32(bgMaxIndex)) + UInt32(bgMinIndex))
+        }
+    }
 }
