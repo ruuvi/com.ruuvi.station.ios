7673c5b1fb5972b90556476550ba282f1c480ec2
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-28 13:19:39 +0500

    show calibration date

--- station/Classes/Presentation/Modules/Calibration/Humidity/Transition/HumidityCalibrationPresentationController.swift
+++ station/Classes/Presentation/Modules/Calibration/Humidity/Transition/HumidityCalibrationPresentationController.swift
@@ -29,7 +29,7 @@ class HumidityCalibrationPresentationController: UIPresentationController {
     }
     
     override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
-        return CGSize(width: 300, height: 322)
+        return CGSize(width: 300, height: 342)
     }
     
     override var frameOfPresentedViewInContainerView: CGRect {
--- station/Classes/Presentation/Modules/Calibration/Humidity/View/HumidityCalibrationViewInput.swift
+++ station/Classes/Presentation/Modules/Calibration/Humidity/View/HumidityCalibrationViewInput.swift
@@ -3,4 +3,5 @@ import Foundation
 protocol HumidityCalibrationViewInput: ViewInput {
     var oldHumidity: Double { get set }
     var humidityOffset: Double { get set }
+    var lastCalibrationDate: Date? { get set }
 }
