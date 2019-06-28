4c75fdc43b2ca91171c563d3fa8a634d5707700d
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-28 11:14:31 +0500

    introduce module HumidityCalibration

---
+++ station/Classes/Presentation/Modules/Calibration/Humidity/Router/HumidityCalibrationRouter.swift
@@ -0,0 +1,9 @@
+import LightRoute
+
+class HumidityCalibrationRouter: HumidityCalibrationRouterInput {
+    weak var transitionHandler: TransitionHandler!
+    
+    func dismiss() {
+        try! transitionHandler.closeCurrentModule().perform()
+    }
+}
