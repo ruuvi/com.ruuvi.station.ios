6cbce48cf21a1c2f9ba0658e63716e00f9cd4f78
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-03 15:24:07 +0500

    add observable

---
+++ station/Classes/Presentation/Binding/Observable.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class Observable<ObservedType> {
+    typealias Observer = (_ observable: Observable<ObservedType>, ObservedType) -> Void
+    
+    private var observers: [Observer]
+    
+    var value: ObservedType? {
+        didSet {
+            if let value = value {
+                notifyObservers(value)
+            }
+        }
+    }
+    
+    init(_ value: ObservedType? = nil) {
+        self.value = value
+        observers = []
+    }
+    
+    func bind(observer: @escaping Observer) {
+        self.observers.append(observer)
+    }
+    
+    private func notifyObservers(_ value: ObservedType) {
+        self.observers.forEach { [unowned self] (observer) in
+            observer(self, value)
+        }
+    }
+}
