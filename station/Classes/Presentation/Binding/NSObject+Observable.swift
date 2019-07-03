381dd02ec371ce38f84c3f1b0bc776fe1ad2bda4
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-03 18:49:24 +0500

    all observable properties should be optionals

--- station/Classes/Presentation/Binding/NSObject+Observable.swift
+++ station/Classes/Presentation/Binding/NSObject+Observable.swift
@@ -1,28 +1,8 @@
 import Foundation
 
-public protocol OptionalType: ExpressibleByNilLiteral {
-    associatedtype WrappedType
-    var asOptional: WrappedType? { get }
-}
-
-extension Optional: OptionalType {
-    public var asOptional: Wrapped? {
-        return self
-    }
-}
-
 extension NSObjectProtocol where Self: NSObject  {
     
     func bind<T: OptionalType>(_ observable: Observable<T>, block: @escaping (Self,T.WrappedType?) -> ()) {
-        block(self, observable.value?.asOptional)
-        observable.bind { [unowned self] observable, value  in
-            DispatchQueue.main.async { [unowned self] in
-                block(self, value?.asOptional)
-            }
-        }
-    }
-    
-    func bind<T>(_ observable: Observable<T>, block: @escaping (Self,T?) -> ()) {
         block(self, observable.value)
         observable.bind { [unowned self] observable, value  in
             DispatchQueue.main.async { [unowned self] in
@@ -30,4 +10,5 @@ extension NSObjectProtocol where Self: NSObject  {
             }
         }
     }
+    
 }
---
+++ station/Classes/Presentation/Binding/Optional.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public protocol OptionalType: ExpressibleByNilLiteral {
+    associatedtype WrappedType
+    var asOptional: WrappedType? { get }
+}
+
+extension Optional: OptionalType {
+    public var asOptional: Wrapped? {
+        return self
+    }
+}
