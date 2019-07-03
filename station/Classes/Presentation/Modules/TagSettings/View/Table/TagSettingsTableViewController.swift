e4d6bc73d31fa5816d5808258b672da0260e43f7
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-03 14:46:51 +0500

    connect outlets

--- station/Classes/Presentation/Modules/TagSettings/View/Table/TagSettingsTableViewController.swift
+++ station/Classes/Presentation/Modules/TagSettings/View/Table/TagSettingsTableViewController.swift
@@ -3,11 +3,23 @@ import UIKit
 class TagSettingsTableViewController: UITableViewController {
     var output: TagSettingsViewOutput!
     
+    @IBOutlet weak var accelerationZValueLabel: UILabel!
+    @IBOutlet weak var accelerationYValueLabel: UILabel!
+    @IBOutlet weak var accelerationXValueLabel: UILabel!
+    @IBOutlet weak var voltageValueLabel: UILabel!
+    @IBOutlet weak var macAddressTitleLabel: UILabel!
+    @IBOutlet weak var macAddressValueLabel: UILabel!
+    @IBOutlet weak var humidityOffsetDate: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var tagNameTextField: UITextField!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.default
     }
+    
 }
 
+// MARK: - TagSettingsViewInput
 extension TagSettingsTableViewController: TagSettingsViewInput {
     func localize() {
         
@@ -15,5 +27,28 @@ extension TagSettingsTableViewController: TagSettingsViewInput {
     
     func apply(theme: Theme) {
         
+    }
+}
+
+// MARK: - IBActions
+extension TagSettingsTableViewController {
+    @IBAction func dismissBarButtonItemAction(_ sender: Any) {
+    }
+    
+    @IBAction func removeThisRuuviTagButtonTouchUpInside(_ sender: Any) {
+    }
+    
+    @IBAction func randomizeBackgroundButtonTouchUpInside(_ sender: Any) {
+    }
+    
+    @IBAction func selectBackgroundButtonTouchUpInside(_ sender: Any) {
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension TagSettingsTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
     }
 }
