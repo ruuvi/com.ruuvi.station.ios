1ead5a1407eb37d38584519d3eaabaa85431aa3c
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-04 17:17:50 +0500

    fix viewmodelmemory leaks

--- station/Classes/Presentation/Binding/NSObject+Observable.swift
+++ station/Classes/Presentation/Binding/NSObject+Observable.swift
@@ -4,7 +4,7 @@ extension NSObjectProtocol where Self: NSObject  {
     
     func bind<T: OptionalType>(_ observable: Observable<T>, block: @escaping (Self,T.WrappedType?) -> ()) {
         block(self, observable.value)
-        observable.bind { [unowned self] observable, value  in
+        observable.bind { [unowned self] _, value  in
             DispatchQueue.main.async { [unowned self] in
                 block(self, value)
             }
