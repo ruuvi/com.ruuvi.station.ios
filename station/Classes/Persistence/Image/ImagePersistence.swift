2d35d4bf5c808e015765dbf7830f52df7504f810
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-06 15:19:14 +0500

    method renames

--- station/Classes/Persistence/Image/ImagePersistence.swift
+++ station/Classes/Persistence/Image/ImagePersistence.swift
@@ -2,7 +2,7 @@ import UIKit
 import Future
 
 protocol ImagePersistence {
-    func fetch(uuid: String) -> UIImage?
-    func delete(uuid: String)
-    func persist(image: UIImage, for uuid: String) -> Future<URL,RUError>
+    func fetchBg(for uuid: String) -> UIImage?
+    func deleteBgIfExists(for uuid: String)
+    func persistBg(image: UIImage, for uuid: String) -> Future<URL,RUError>
 }
