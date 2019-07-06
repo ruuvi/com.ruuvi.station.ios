7818788d422cb97279a81de4170042e54144d742
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-06 15:47:14 +0500

    start stop saving in bg

--- station/Classes/Application/AppDelegate.swift
+++ station/Classes/Application/AppDelegate.swift
@@ -6,6 +6,7 @@ import Crashlytics
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var appStateService: AppStateService!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Crashlytics().debugMode = true
@@ -17,25 +18,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let mainRouter = MainRouter.shared
             mainRouter.openDashboard()
         }
+        appStateService = r.resolve(AppStateService.self)
+        appStateService.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        appStateService.applicationWillResignActive(application)
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        appStateService.applicationDidEnterBackground(application)
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        appStateService.applicationWillEnterForeground(application)
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        appStateService.applicationDidBecomeActive(application)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
