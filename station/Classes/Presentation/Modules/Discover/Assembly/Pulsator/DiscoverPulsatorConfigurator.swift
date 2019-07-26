12c2cd520386783d3b7df74059db6af57874c7e0
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-26 16:46:31 +0500

    inject permissions manager

--- station/Classes/Presentation/Modules/Discover/Assembly/Pulsator/DiscoverPulsatorConfigurator.swift
+++ station/Classes/Presentation/Modules/Discover/Assembly/Pulsator/DiscoverPulsatorConfigurator.swift
@@ -14,6 +14,8 @@ class DiscoverPulsatorConfigurator {
         presenter.realmContext = r.resolve(RealmContext.self)
         presenter.errorPresenter = r.resolve(ErrorPresenter.self)
         presenter.ruuviTagService = r.resolve(RuuviTagService.self)
+        presenter.webTagService = r.resolve(WebTagService.self)
+        presenter.permissionsManager = r.resolve(PermissionsManager.self)
         presenter.scanner = r.resolve(BTScanner.self)
         
         view.output = presenter
