3dbb4692d30ac8a8165e5efd6f897d7a5e584152
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-31 16:48:38 +0500

    add func update(location: Location, of webTag: WebTagRealm) -> Future<Bool,RUError>

--- station/Classes/Persistence/WebTag/Realm/WebTagPersistenceRealm.swift
+++ station/Classes/Persistence/WebTag/Realm/WebTagPersistenceRealm.swift
@@ -5,6 +5,50 @@ class WebTagPersistenceRealm: WebTagPersistence {
     
     var context: RealmContext!
     
+    func update(location: Location, of webTag: WebTagRealm) -> Future<Bool,RUError> {
+        let promise = Promise<Bool,RUError>()
+        if webTag.realm == context.bg {
+            context.bgWorker.enqueue {
+                do {
+                    try self.context.bg.write {
+                        if let oldLocation = webTag.location {
+                            self.context.bg.delete(oldLocation)
+                        }
+                        let newLocation = WebTagLocationRealm()
+                        newLocation.city = location.city
+                        newLocation.country = location.country
+                        newLocation.latitude = location.coordinate.latitude
+                        newLocation.longitude = location.coordinate.longitude
+                        self.context.bg.add(newLocation)
+                        webTag.location = newLocation
+                    }
+                    promise.succeed(value: true)
+                } catch {
+                    promise.fail(error: .persistence(error))
+                }
+            }
+        } else {
+            do {
+                try context.main.write {
+                    if let oldLocation = webTag.location {
+                        self.context.main.delete(oldLocation)
+                    }
+                    let newLocation = WebTagLocationRealm()
+                    newLocation.city = location.city
+                    newLocation.country = location.country
+                    newLocation.latitude = location.coordinate.latitude
+                    newLocation.longitude = location.coordinate.longitude
+                    self.context.main.add(newLocation)
+                    webTag.location = newLocation
+                }
+                promise.succeed(value: true)
+            } catch {
+                promise.fail(error: .persistence(error))
+            }
+        }
+        return promise.future
+    }
+    
     func persist(provider: WeatherProvider) -> Future<WeatherProvider,RUError> {
         let promise = Promise<WeatherProvider,RUError>()
         context.bgWorker.enqueue {
--- station/Classes/Persistence/WebTag/WebTagPersistence.swift
+++ station/Classes/Persistence/WebTag/WebTagPersistence.swift
@@ -5,4 +5,5 @@ protocol WebTagPersistence {
     func persist(provider: WeatherProvider) -> Future<WeatherProvider,RUError>
     func remove(webTag: WebTagRealm) -> Future<Bool,RUError>
     func update(name: String, of webTag: WebTagRealm) -> Future<Bool,RUError>
+    func update(location: Location, of webTag: WebTagRealm) -> Future<Bool,RUError>
 }
