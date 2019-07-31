b5e603ba75d9e8e56d08ff49ad72c35c440d65bd
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-31 16:36:26 +0500

    add webTagLocationRealm

--- station/Classes/Model/Realm/WebTagRealm.swift
+++ station/Classes/Model/Realm/WebTagRealm.swift
@@ -4,6 +4,7 @@ class WebTagRealm: Object {
     @objc dynamic var name: String = ""
     @objc dynamic var uuid: String = ""
     @objc dynamic var providerString: String = WeatherProvider.openWeatherMap.rawValue
+    let data = LinkingObjects(fromType: WebTagLocationRealm.self, property: "webTag")
     
     var provider: WeatherProvider {
         if let provider = WeatherProvider(rawValue: providerString) {
@@ -23,4 +24,14 @@ class WebTagRealm: Object {
         self.providerString = provider.rawValue
         self.name = provider.displayName
     }
+}
+
+class WebTagLocationRealm: Object {
+    
+    @objc dynamic var webTag: WebTagRealm?
+    
+    @objc dynamic var city: String?
+    @objc dynamic var country: String?
+    @objc dynamic var latitude: Double = 0
+    @objc dynamic var longitude: Double = 0
 }
