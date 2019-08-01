da892a21ed9f3891c917b3123db2d938518bff33
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-08-01 16:18:24 +0500

    move weather methods to weather provider service

--- station/Classes/Business/Services/WebTag/WebTagService.swift
+++ station/Classes/Business/Services/WebTag/WebTagService.swift
@@ -9,35 +9,4 @@ protocol WebTagService {
     func update(location: Location, of webTag: WebTagRealm) -> Future<Bool,RUError>
     func clearLocation(of webTag: WebTagRealm) -> Future<Bool,RUError>
     
-    func loadCurrentLocationData(from provider: WeatherProvider) -> Future<WebTagData,RUError>
-    
-    @discardableResult
-    func observeCurrentLocationData<T: AnyObject>(_ observer: T, provider: WeatherProvider, interval: TimeInterval, closure: @escaping (T, WebTagData?, RUError?) -> Void) -> WebTagServiceObservationToken
-    
-}
-
-class WebTagServiceObservationToken {
-    private let cancellationClosure: () -> Void
-    
-    init(cancellationClosure: @escaping () -> Void) {
-        self.cancellationClosure = cancellationClosure
-    }
-    
-    public func invalidate() {
-        cancellationClosure()
-    }
-}
-
-struct WebTagData {
-    var celsius: Double?
-    var humidity: Double?
-    var pressure: Double?
-    
-    var fahrenheit: Double? {
-        if let celsius = celsius {
-            return (celsius * 9.0/5.0) + 32.0
-        } else {
-            return nil
-        }
-    }
 }
