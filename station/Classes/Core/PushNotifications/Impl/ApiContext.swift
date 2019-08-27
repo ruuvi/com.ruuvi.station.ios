12e28dee494946441860fe24320be36e353d2005
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-08-27 09:12:58 +0500

    add ApiContext

---
+++ station/Classes/Core/PushNotifications/Impl/ApiContext.swift
@@ -0,0 +1,34 @@
+//
+//  ApiContext.swift
+//  iWallet
+//
+//  Created by Rinat Enikeev on 8/27/19.
+//  Copyright © 2019 Suffescom. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ApiContext {
+    static let shared = ApiContext()
+    let baseUrl = "https://staging.iwallet.com/api/v1/app/"
+    var sessionManager: SessionManager
+    
+    init() {
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            "staging.iwallet.com": .pinPublicKeys(
+                publicKeys: ServerTrustPolicy.publicKeys(),
+                validateCertificateChain: true,
+                validateHost: true
+            )
+        ]
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        self.sessionManager = SessionManager(
+            configuration: configuration,
+            serverTrustPolicyManager: ServerTrustPolicyManager(
+                policies: serverTrustPolicies
+            )
+        )
+    }
+}
