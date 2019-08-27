f097f5c996871a786ebc0170d4defd138c06de8c
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-08-27 13:10:08 +0500

    add kelvin temperature unit to settings

--- station/Classes/Presentation/Modules/Settings/View/SettingsViewOutput.swift
+++ station/Classes/Presentation/Modules/Settings/View/SettingsViewOutput.swift
@@ -4,6 +4,5 @@ protocol SettingsViewOutput {
     func viewDidLoad()
     func viewDidChange(temperatureUnit: TemperatureUnit)
     func viewDidChange(humidityUnit: HumidityUnit)
-    func viewDidChange(experimentalUX: Bool)
     func viewDidTriggerClose()
 }
