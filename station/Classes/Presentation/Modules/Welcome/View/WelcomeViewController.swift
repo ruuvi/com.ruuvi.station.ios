1d568268563cef7aacae0234fc542f98caaba47f
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-02 14:56:25 +0500

    localize welcome screen (without logo)

--- station/Classes/Presentation/Modules/Welcome/View/WelcomeViewController.swift
+++ station/Classes/Presentation/Modules/Welcome/View/WelcomeViewController.swift
@@ -3,14 +3,20 @@ import UIKit
 class WelcomeViewController: UIViewController {
     var output: WelcomeViewOutput!
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var scanButton: UIButton!
+    @IBOutlet weak var welcomeImageView: UIImageView!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
 }
 
 extension WelcomeViewController: WelcomeViewInput {
     func localize() {
-        
+        descriptionLabel.text = "Welcome.description.text".localized()
+        scanButton.setTitle("Welcome.scan.title".localized(), for: .normal)
     }
     
     func apply(theme: Theme) {
