24e1a10bbc195d6c7b28eb2640ad35aca54fa12d
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-03 20:38:03 +0500

    finish localization of welcome screen

--- station/Classes/Presentation/Contract/ViewInput.swift
+++ station/Classes/Presentation/Contract/ViewInput.swift
@@ -3,3 +3,9 @@ import Foundation
 protocol ViewInput: Themeable, Localizable {
     
 }
+
+extension ViewInput {
+    func setupLocalization() {
+        LocalizationService.shared.add(localizable: self)
+    }
+}
