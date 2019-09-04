1a250f48618d3c6cfcec28ab6535bf3b683d823b
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-04 10:23:54 +0500

    localize ruuvi ago string

--- station/Extensions/Date+Ruuvi.swift
+++ station/Extensions/Date+Ruuvi.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Date {
     var ruuviAgo: String {
         let elapsed = Int(Date().timeIntervalSince(self))
-        var output = "Updated "
+        var output = "Updated".localized() + " "
         // show date if the tag has not been seen for 24h
         if elapsed >= 24 * 60 * 60 {
             let df = DateFormatter()
@@ -14,12 +14,12 @@ extension Date {
             let minutes = (elapsed / 60) % 60
             let hours   = (elapsed / (60*60)) % 24
             if hours > 0 {
-                output += String(hours) + " h "
+                output += String(hours) + " " + "h".localized() + " "
             }
             if minutes > 0 {
-                output += String(minutes) + " min "
+                output += String(minutes) + " " + "min".localized() + " "
             }
-            output += String(seconds) + " s ago"
+            output += String(seconds) + " " + "s".localized() + " " + "ago".localized()
         }
         return output
     }
