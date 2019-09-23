17721a27d829dcfb72f2b86d52bcebfa41d0dc1f
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-23 23:03:32 +0500

    add relative humidity to view model

--- station/Classes/Presentation/Modules/Dashboard/Charts/View/TagChartsViewModel.swift
+++ station/Classes/Presentation/Modules/Dashboard/Charts/View/TagChartsViewModel.swift
@@ -20,27 +20,41 @@ struct TagChartsViewModel {
     var kelvin: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
     var temperatureUnit: Observable<TemperatureUnit?> = Observable<TemperatureUnit?>()
     var humidityUnit: Observable<HumidityUnit?> = Observable<HumidityUnit?>()
+    var relativeHumidity: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
+    var absoluteHumidity: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
+    var dewPointCelsius: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
+    var dewPointFahrenheit: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
+    var dewPointKelvin: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
+    var humidityOffset: Observable<Double?> = Observable<Double?>(0)
+    var pressure: Observable<[TagChartsPoint]?> = Observable<[TagChartsPoint]?>()
     
     init(_ ruuviTag: RuuviTagRealm) {
         type = .ruuvi
         uuid.value = ruuviTag.uuid
         name.value = ruuviTag.name
-        celsius.value = ruuviTag.data.compactMap({
+        celsius.value = ruuviTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.celsius.value {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
             }
         })
-        fahrenheit.value = ruuviTag.data.compactMap({
+        fahrenheit.value = ruuviTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.fahrenheit {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
             }
         })
-        kelvin.value = ruuviTag.data.compactMap({
+        kelvin.value = ruuviTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.kelvin {
+                return TagChartsPoint(date: $0.date, value: value)
+            } else {
+                return nil
+            }
+        })
+        relativeHumidity.value = ruuviTag.data.sorted(byKeyPath: "date").compactMap({
+            if let value = $0.humidity.value {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
@@ -52,22 +66,29 @@ struct TagChartsViewModel {
         type = .web
         uuid.value = webTag.uuid
         name.value = webTag.name
-        celsius.value = webTag.data.compactMap({
+        celsius.value = webTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.celsius.value {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
             }
         })
-        fahrenheit.value = webTag.data.compactMap({
+        fahrenheit.value = webTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.fahrenheit {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
             }
         })
-        kelvin.value = webTag.data.compactMap({
+        kelvin.value = webTag.data.sorted(byKeyPath: "date").compactMap({
             if let value = $0.kelvin {
+                return TagChartsPoint(date: $0.date, value: value)
+            } else {
+                return nil
+            }
+        })
+        relativeHumidity.value = webTag.data.sorted(byKeyPath: "date").compactMap({
+            if let value = $0.humidity.value {
                 return TagChartsPoint(date: $0.date, value: value)
             } else {
                 return nil
