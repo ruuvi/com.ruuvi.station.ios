424ecc80adec15eadbe9fa8ad9ac8dd28bffa043
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-23 23:04:55 +0500

    weak instead of unwoned

--- station/Classes/Presentation/Binding/NSObject+Observable.swift
+++ station/Classes/Presentation/Binding/NSObject+Observable.swift
@@ -4,9 +4,10 @@ extension NSObjectProtocol where Self: NSObject  {
     
     func bind<T: OptionalType>(_ observable: Observable<T>, block: @escaping (Self,T.WrappedType?) -> ()) {
         block(self, observable.value)
-        observable.bind { [unowned self] _, value  in
-            DispatchQueue.main.async { [unowned self] in
-                block(self, value)
+        observable.bind { [weak self] _, value  in
+            DispatchQueue.main.async { [weak self] in
+                guard let sSelf = self else { return }
+                block(sSelf, value)
             }
         }
     }
