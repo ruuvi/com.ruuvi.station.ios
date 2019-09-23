9a2b7ad02bb7998ff6cd9ab35ac0c3bbe1977692
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-09-23 16:18:47 +0500

    style and show temperature chart

--- station/Classes/Presentation/Modules/Dashboard/Charts/View/Scroll/TagChartsView.swift
+++ station/Classes/Presentation/Modules/Dashboard/Charts/View/Scroll/TagChartsView.swift
@@ -1,6 +1,10 @@
 import UIKit
+import Charts
 
 class TagChartsView: UIView {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var temperatureChart: LineChartView!
+    @IBOutlet weak var humidityChart: LineChartView!
+    @IBOutlet weak var pressureChart: LineChartView!
 }
