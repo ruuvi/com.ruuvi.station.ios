df2385870c569e56fadaee9aba28b1bedcf18fa8
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-10-12 11:14:30 +0500

    add interval for daemons

--- station/Classes/Presentation/Modules/Settings/Submodules/Daemons/Presenter/DaemonsPresenter.swift
+++ station/Classes/Presentation/Modules/Settings/Submodules/Daemons/Presenter/DaemonsPresenter.swift
@@ -15,15 +15,23 @@ class DaemonsPresenter: NSObject, DaemonsModuleInput {
         let advertisement = DaemonsViewModel()
         advertisement.type = .advertisement
         advertisement.isOn.value = settings.isAdvertisementDaemonOn
+        advertisement.interval.value = settings.advertisementDaemonIntervalMinutes
         bind(advertisement.isOn, fire: false) { (observer, isOn) in
             observer.settings.isAdvertisementDaemonOn = isOn ?? true
+        }
+        bind(advertisement.interval, fire: false) { observer, interval in
+            observer.settings.advertisementDaemonIntervalMinutes = interval ?? 1
         }
         
         let connection = DaemonsViewModel()
         connection.type = .connection
         connection.isOn.value = settings.isConnectionDaemonOn
+        connection.interval.value = settings.connectionDaemonIntervalMinutes
         bind(connection.isOn, fire: false) { (observer, isOn) in
             observer.settings.isConnectionDaemonOn = isOn ?? true
+        }
+        bind(connection.interval, fire: false) { observer, interval in
+            observer.settings.connectionDaemonIntervalMinutes = interval ?? 1
         }
         
         view.viewModels = [advertisement, connection]
--- station/Classes/Presentation/Modules/Settings/Submodules/Daemons/SwiftUI/DaemonsList.swift
+++ station/Classes/Presentation/Modules/Settings/Submodules/Daemons/SwiftUI/DaemonsList.swift
@@ -5,11 +5,18 @@ struct DaemonsList: View {
     
     @EnvironmentObject var env: DaemonsEnvironmentObject
     
+    func index(of daemon: DaemonsViewModel) -> Int {
+        return env.daemons.firstIndex(where: { $0.id == daemon.id })!
+    }
+    
     var body: some View {
         List {
             ForEach(env.daemons) { daemon in
                 Section(header: Text(daemon.section)) {
                     DaemonsRow(daemon: daemon)
+                    if daemon.isOn.value.bound {
+                        Stepper("Every" + " " + "\(daemon.interval.value.bound)" + " " + "min", value: self.$env.daemons[self.index(of: daemon)].interval.value.bound, in: 1...1000)
+                    }
                 }
             }
             
