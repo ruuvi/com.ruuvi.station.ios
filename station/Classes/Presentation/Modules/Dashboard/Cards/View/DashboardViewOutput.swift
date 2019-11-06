d09bcedce35cc1af1623b7a3fdf9c6662dd27cdc
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-06 13:40:03 +0500

    add view did scroll to view model  method

--- station/Classes/Presentation/Modules/Dashboard/Cards/View/DashboardViewOutput.swift
+++ station/Classes/Presentation/Modules/Dashboard/Cards/View/DashboardViewOutput.swift
@@ -5,6 +5,7 @@ protocol DashboardViewOutput {
     func viewWillAppear()
     func viewWillDisappear()
     func viewDidTriggerMenu()
+    func viewDidScroll(to viewModel: DashboardTagViewModel)
     func viewDidTriggerSettings(for viewModel: DashboardTagViewModel)
     func viewDidTriggerChart(for viewModel: DashboardTagViewModel)
 }
