98226d28dc781253ebcf01d4f6a4bbb87158a246
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-08 19:32:45 +0500

    register alert service and add isEnabled handling

--- station/Classes/Presentation/Modules/TagSettings/View/Table/RangeSeekSlider/RURangeSeekSlider.swift
+++ station/Classes/Presentation/Modules/TagSettings/View/Table/RangeSeekSlider/RURangeSeekSlider.swift
@@ -1,9 +1,18 @@
 import RangeSeekSlider
 
 class RURangeSeekSlider: RangeSeekSlider {
+    private let ruuviColor = UIColor(red: 21.0/255.0, green: 141.0/255.0, blue: 165.0/255.0, alpha: 1.0)
+    
     override var isEnabled: Bool {
         didSet {
-            print("rot")
+            if isEnabled {
+                handleColor = ruuviColor
+                colorBetweenHandles = ruuviColor
+            } else {
+                handleColor = .darkGray
+                colorBetweenHandles = .darkGray
+            }
+            minValue = minValue // hack to refresh
         }
     }
     
