5d02a8cda0e02c06a1e63f6f7032d751ef118ba9
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-19 23:38:51 +0500

    show welcome shown default

--- station/Classes/Presentation/Modules/Settings/Submodules/Defaults/View/Table/DefaultsTableViewController.swift
+++ station/Classes/Presentation/Modules/Settings/Submodules/Defaults/View/Table/DefaultsTableViewController.swift
@@ -11,6 +11,8 @@ class DefaultsTableViewController: UITableViewController {
         }
     }
 
+    private let switchCellReuseIdentifier = "DefaultsSwitchTableViewCellReuseIdentifier"
+    
 }
 
 extension DefaultsTableViewController: DefaultsViewInput {
@@ -22,3 +24,38 @@ extension DefaultsTableViewController: DefaultsViewInput {
         
     }
 }
+
+// MARK: - View lifecycle
+extension DefaultsTableViewController {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension DefaultsTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModels.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let viewModel = viewModels[indexPath.section]
+        let cell = tableView.dequeueReusableCell(withIdentifier: switchCellReuseIdentifier, for: indexPath) as! DefaultsSwitchTableViewCell
+        cell.titleLabel.text = viewModel.title
+        cell.isOnSwitch.isOn = viewModel.boolean.value.bound
+        cell.delegate = self
+        return cell
+    }
+}
+
+// MARK: - DefaultsSwitchTableViewCellDelegate
+extension DefaultsTableViewController: DefaultsSwitchTableViewCellDelegate {
+    func defaultsSwitch(cell: DefaultsSwitchTableViewCell, didChange value: Bool) {
+        if let indexPath = tableView.indexPath(for: cell) {
+            viewModels[indexPath.section].boolean.value = value
+        }
+    }
+}
+
