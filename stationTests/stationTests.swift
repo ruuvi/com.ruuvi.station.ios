3c74b1138a8dd2a809059cba4314cf91c64b3c48
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-12-06 11:13:16 +0500

    fix crash using unit tests

--- stationTests/stationTests.swift
+++ stationTests/stationTests.swift
@@ -1,22 +1,19 @@
 import XCTest
+@testable import station
 
 class StationTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testWebTagDaemonCrash() {
+        NotificationCenter
+        .default
+        .post(name: .isWebTagDaemonOnDidChange,
+              object: self,
+              userInfo: nil)
+        NotificationCenter
+        .default
+        .post(name: .WebTagDaemonIntervalDidChange,
+         object: self,
+         userInfo: nil)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
