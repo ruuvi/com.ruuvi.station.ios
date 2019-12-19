dc15eb909cbda3de16529ceaad64e135b4e32b0a
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-12-19 11:36:39 +0500

    refactor PermissionPresenterAlert

--- station/Classes/Presentation/Presenters/Permission/Alert/PermissionPresenterAlert.swift
+++ station/Classes/Presentation/Presenters/Permission/Alert/PermissionPresenterAlert.swift
@@ -1,57 +1,29 @@
 import UIKit
 
 class PermissionPresenterAlert: PermissionPresenter {
+    
     func presentNoPhotoLibraryPermission() {
-        guard let viewController = UIApplication.shared.topViewController() else { return }
         let message = "PermissionPresenter.NoPhotoLibraryAccess.message".localized()
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let actionTitle = "PermissionPresenter.settings".localized()
-        let settings = UIAlertAction(title: actionTitle, style: .default) { _ -> Void in
-            if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(settingsUrl, options: [:])
-            }
-        }
-        let cancel = UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil)
-        alert.addAction(settings)
-        alert.addAction(cancel)
-        viewController.present(alert, animated: true)
+        presentAlert(with: message)
     }
 
     func presentNoCameraPermission() {
-        guard let viewController = UIApplication.shared.topViewController() else { return }
         let message = "PermissionPresenter.NoCameraAccess.message".localized()
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil)
-        let actionTitle = "PermissionPresenter.settings".localized()
-        let settings = UIAlertAction(title: actionTitle, style: .default) { (_) -> Void in
-            if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(settingsUrl, options: [:])
-            }
-        }
-        alert.addAction(settings)
-        alert.addAction(cancel)
-        viewController.present(alert, animated: true)
+        presentAlert(with: message)
     }
 
     func presentNoLocationPermission() {
-        guard let viewController = UIApplication.shared.topViewController() else { return }
         let message = "PermissionPresenter.NoLocationAccess.message".localized()
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil)
-        let actionTitle = "PermissionPresenter.settings".localized()
-        let settings = UIAlertAction(title: actionTitle, style: .default) { _ -> Void in
-            if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(settingsUrl, options: [:])
-            }
-        }
-        alert.addAction(settings)
-        alert.addAction(cancel)
-        viewController.present(alert, animated: true)
+        presentAlert(with: message)
     }
 
     func presentNoPushNotificationsPermission() {
-        guard let viewController = UIApplication.shared.topViewController() else { return }
         let message = "PermissionPresenter.NoPushNotificationsPermission.message".localized()
+        presentAlert(with: message)
+    }
+
+    private func presentAlert(with message: String) {
+        guard let viewController = UIApplication.shared.topViewController() else { return }
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let cancel = UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil)
         let actionTitle = "PermissionPresenter.settings".localized()
