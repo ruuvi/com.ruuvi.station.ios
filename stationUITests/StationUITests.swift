be1ecfa33e35cdf24a850fa7f6b5e7e9788b89d9
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-01-18 14:22:27 +0500

    update station ui tests

---
+++ stationUITests/StationUITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+class StationUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
