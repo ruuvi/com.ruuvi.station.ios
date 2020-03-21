75dad0ea0177d07a31823be594a6767cd4c240ac
Viik.ufa <viik.ufa@Ufa-YamschikovVS.local>
2020-03-21 17:31:03 +0500

    Create measurements models
    * created RuuviMeasurement with use Foundation MeasurementTypes
    * added extension for RuuviTagDataRealm

---
+++ station/Classes/Model/Measurement/RuuviMeasurement.swift
@@ -0,0 +1,37 @@
+//
+//  Measurement.swift
+//  station
+//
+//  Created by Viik.ufa on 21.03.2020.
+//  Copyright © 2020 Ruuvi Innovations Oy. BSD-3-Clause.
+//
+
+import Foundation
+import Humidity
+
+typealias Temperature = Measurement<UnitTemperature>
+typealias Pressure = Measurement<UnitPressure>
+typealias Voltage = Measurement<UnitElectricPotentialDifference>
+typealias AccelerationMeasurement = Measurement<UnitAcceleration>
+
+struct Acceleration {
+    let x: AccelerationMeasurement
+    let y: AccelerationMeasurement
+    let z: AccelerationMeasurement
+}
+
+struct RuuviMeasurement {
+    let tagUuid: String
+    let measurementSequenceNumber: Int
+    let date: Date
+    let rssi: Int?
+    let temperature: Temperature?
+    let humidity: Humidity?
+    let pressure: Pressure?
+    // v3 & v5
+    let acceleration: Acceleration?
+    let voltage: Voltage?
+    // v5
+    let movementCounter: Int?
+    let txPower: Int?
+}
