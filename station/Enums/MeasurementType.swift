4e5fecd0c3371cf59f524dfd5a67726e8d1ee6b1
Viik.ufa <viik.ufa@Ufa-YamschikovVS.local>
2020-03-21 17:31:03 +0500

    Create measurements models
    * created RuuviMeasurement with use Foundation MeasurementTypes
    * added extension for RuuviTagDataRealm

---
+++ station/Enums/MeasurementType.swift
@@ -0,0 +1,12 @@
+enum MeasurementType {
+    case rssi
+    case temperature
+    case humidity
+    case pressure
+    // v3 & v5
+    case acceleration
+    case voltage
+    // v5
+    case movementCounter
+    case txPower
+}
