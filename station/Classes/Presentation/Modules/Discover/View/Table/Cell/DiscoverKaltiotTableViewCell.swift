e67239c00a02dc2bd48016f2393b53f7e119fb47
Vitaliy Yamshchikov <viik.ufa@gmail.com>
2020-04-23 00:49:47 +0500

    Added DiscoverKaltiotCell
    * added tableView extensions

---
+++ station/Classes/Presentation/Modules/Discover/View/Table/Cell/DiscoverKaltiotTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DiscoverKaltiotTableViewCell.swift
+//  station
+//
+//  Created by Viik.ufa on 22.04.2020.
+//  Copyright © 2020 Ruuvi Innovations Oy. BSD-3-Clause.
+//
+
+import UIKit
+
+class DiscoverKaltiotTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
