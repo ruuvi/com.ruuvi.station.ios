21098f620ff698ed2c73595609ad320bfb12c035
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-05-03 17:17:53 +0300

    add observe method to reactor for ruuviTagId

--- station/Classes/Reactor/RuuviTag/Impl/RuuviTagReactorImpl.swift
+++ station/Classes/Reactor/RuuviTag/Impl/RuuviTagReactorImpl.swift
@@ -13,10 +13,97 @@ class RuuviTagReactorImpl: RuuviTagReactor {
     var realmPersistence: RuuviTagPersistenceRealm!
 
     private lazy var entityRxSwift = RuuviTagSubjectRxSwift(sqlite: sqliteContext, realm: realmContext)
+    private lazy var recordRxSwifts = [String: RuuviTagRecordSubjectRxSwift]()
     #if canImport(Combine)
     @available(iOS 13, *)
     private lazy var entityCombine = RuuviTagSubjectCombine(sqlite: sqliteContext, realm: realmContext)
+    @available(iOS 13, *)
+    private lazy var recordCombines = [String: RuuviTagRecordSubjectCombine]()
     #endif
+
+    func observe(_ ruuviTagId: String,
+                 _ block: @escaping (ReactorChange<RuuviTagSensorRecord>) -> Void) -> RUObservationToken {
+        let sqliteOperation = sqlitePersistence.readAll(ruuviTagId)
+        let realmOperation = realmPersistence.readAll(ruuviTagId)
+        Future.zip(realmOperation, sqliteOperation).on(success: { realmRecords, sqliteRecords in
+            block(.initial(sqliteRecords + realmRecords))
+        }, failure: { error in
+            block(.error(error))
+        })
+
+        #if canImport(Combine)
+        if #available(iOS 13, *) {
+            var recordCombine: RuuviTagRecordSubjectCombine
+            if let combine = recordCombines[ruuviTagId] {
+                recordCombine = combine
+            } else {
+                let combine = RuuviTagRecordSubjectCombine(ruuviTagId: ruuviTagId, sqlite: sqliteContext, realm: realmContext)
+                recordCombines[ruuviTagId] = combine
+                recordCombine = combine
+            }
+            let insert = recordCombine.insertSubject.sink { value in
+                block(.insert(value))
+            }
+            let update = recordCombine.updateSubject.sink { value in
+                block(.update(value))
+            }
+            let delete = recordCombine.deleteSubject.sink { value in
+                block(.delete(value))
+            }
+            return RUObservationToken {
+                insert.cancel()
+                update.cancel()
+                delete.cancel()
+            }
+        } else {
+            var recordRxSwift: RuuviTagRecordSubjectRxSwift
+            if let rxSwift = recordRxSwifts[ruuviTagId] {
+                recordRxSwift = rxSwift
+            } else {
+                let rxSwift = RuuviTagRecordSubjectRxSwift(ruuviTagId: ruuviTagId, sqlite: sqliteContext, realm: realmContext)
+                recordRxSwifts[ruuviTagId] = rxSwift
+                recordRxSwift = rxSwift
+            }
+            let insert = recordRxSwift.insertSubject.subscribe(onNext: { value in
+                block(.insert(value))
+            })
+            let update = recordRxSwift.updateSubject.subscribe(onNext: { value in
+                block(.update(value))
+            })
+            let delete = recordRxSwift.deleteSubject.subscribe(onNext: { value in
+                block(.delete(value))
+            })
+            return RUObservationToken {
+                insert.dispose()
+                update.dispose()
+                delete.dispose()
+            }
+        }
+        #else
+        var recordRxSwift: RuuviTagRecordSubjectRxSwift
+        if let rxSwift = recordRxSwifts[ruuviTagId] {
+            recordRxSwift = rxSwift
+        } else {
+            let rxSwift = RuuviTagRecordSubjectRxSwift(ruuviTagId: ruuviTagId, sqlite: sqliteContext, realm: realmContext)
+            recordRxSwifts[ruuviTagId] = rxSwift
+            recordRxSwift = rxSwift
+        }
+        let insert = recordRxSwift.insertSubject.subscribe(onNext: { value in
+            block(.insert(value))
+        })
+        let update = recordRxSwift.updateSubject.subscribe(onNext: { value in
+            block(.update(value))
+        })
+        let delete = recordRxSwift.deleteSubject.subscribe(onNext: { value in
+            block(.delete(value))
+        })
+        return RUObservationToken {
+            insert.dispose()
+            update.dispose()
+            delete.dispose()
+        }
+        #endif
+    }
 
     func observe(_ block: @escaping (ReactorChange<RuuviTagSensor>) -> Void) -> RUObservationToken {
         let sqliteOperation = sqlitePersistence.readAll()
@@ -60,13 +147,13 @@ class RuuviTagReactorImpl: RuuviTagReactor {
             }
         }
         #else
-        let insert = rxSwift.insertSubject.subscribe(onNext: { value in
+        let insert = entityRxSwift.insertSubject.subscribe(onNext: { value in
             block(.insert(value))
         })
-        let update = rxSwift.updateSubject.subscribe(onNext: { value in
+        let update = entityRxSwift.updateSubject.subscribe(onNext: { value in
             block(.update(value))
         })
-        let delete = rxSwift.deleteSubject.subscribe(onNext: { value in
+        let delete = entityRxSwift.deleteSubject.subscribe(onNext: { value in
             block(.delete(value))
         })
         return RUObservationToken {
