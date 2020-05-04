fd540dfdee7ebb0bbd232a5cab06312e992c7c09
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-05-04 13:33:46 +0300

    eliminate RuuviTagService

--- station/Classes/Business/Services/RuuviTag/RuuviTagService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Future
-import BTKit
-
-protocol RuuviTagService {
-    func delete(ruuviTag: RuuviTagRealm) -> Future<Bool, RUError>
-    func update(name: String, of ruuviTag: RuuviTagRealm) -> Future<Bool, RUError>
-}
