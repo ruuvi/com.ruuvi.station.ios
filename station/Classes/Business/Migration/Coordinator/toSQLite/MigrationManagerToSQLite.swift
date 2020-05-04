2f2f0bca2aed4fbe020b442508a2678d1a9258b5
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-05-04 14:52:39 +0300

    prepare properties of migration manager

--- station/Classes/Business/Migration/Coordinator/toSQLite/MigrationManagerToSQLite.swift
+++ station/Classes/Business/Migration/Coordinator/toSQLite/MigrationManagerToSQLite.swift
@@ -1,7 +1,22 @@
 import Foundation
+import RealmSwift
 
 class MigrationManagerToSQLite: MigrationManager {
+
+    // persistence
+    var alert: AlertPersistence!
+    var background: BackgroundPersistence!
+    var calibration: CalibrationPersistence!
+    var connection: ConnectionPersistence!
+    var id: IDPersistence!
+    var image: ImagePersistence!
+    var settings: Settings!
+
+    // context
+    var realm: RealmContext!
+    var sqlite: SQLiteContext!
+
     func migrateIfNeeded() {
-        print("ROT")
+
     }
 }
