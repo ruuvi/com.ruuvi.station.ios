a32cf337cb4297e1929ea22ac5069ce6e4c96d9d
Vitaliy Yamshchikov <viik.ufa@gmail.com>
2020-08-11 21:55:08 +0500

    IOS-343 use calibration humidity offset for charts humidity values

--- station/Classes/Presentation/Modules/Dashboard/Charts/Interactor/TagChartsInteractor.swift
+++ station/Classes/Presentation/Modules/Dashboard/Charts/Interactor/TagChartsInteractor.swift
@@ -41,7 +41,7 @@ class TagChartsInteractor {
         MeasurementType.chartsCases.forEach({
             let viewModel = TagChartViewModel(type: $0)
             let module = TagChartAssembler.createModule()
-            module.configure(viewModel, output: self)
+            module.configure(viewModel, output: self, luid: ruuviTagSensor.luid)
             chartModules.append(module)
         })
     }
