f3c43a43f6a67a496bde1f1cba7f24839fcadf2b
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-04-08 18:40:49 +0500

    introduce feature toggle source and local provider

--- station/Classes/Business/Features/Providers/LocalFeatureToggleProvider.swift
+++ station/Classes/Business/Features/Providers/LocalFeatureToggleProvider.swift
@@ -2,48 +2,12 @@ import Foundation
 
 public final class LocalFeatureToggleProvider: FeatureToggleProvider {
     public func fetchFeatureToggles(_ completion: @escaping FeatureToggleCallback) {
-        let configuration = LocalFeatureToggleProvider.loadConfiguration() ?? []
-
-        completion(configuration)
-    }
-}
-
-extension LocalFeatureToggleProvider {
-    static let jsonContainerName: String = "featureToggles"
-    static let configurationName: String = "FeatureToggles"
-    static let configurationType: String = "json"
-
-    static func loadConfiguration() -> [FeatureToggle]? {
-        guard let configurationURL = bundledConfigurationURL(),
-              let data = try? Data(contentsOf: configurationURL) else {
-            return nil
+        let featureToggles: [FeatureToggle] = Feature.allCases.map { feature in
+            let isEnabled = UserDefaults.standard.bool(forKey: isEnabledPrefix + feature.rawValue)
+            return FeatureToggle(feature: feature, enabled: isEnabled, source: .local)
         }
-        return parseConfiguration(data: data)
+        completion(featureToggles)
     }
-    static func parseConfiguration(data: Data) -> ParsingServiceResult? {
-        return JSONParsingService().parse(data, containerName: jsonContainerName)
-    }
-    static func bundledConfigurationURL() -> URL? {
-        return Bundle.main.url(forResource: configurationName, withExtension: configurationType)
-    }
-}
 
-typealias ParsingServiceResult = [FeatureToggle]
-
-protocol ParsingService {
-    func parse(_ data: Data, containerName: String) -> ParsingServiceResult?
-}
-public struct JSONParsingService: ParsingService {
-    func parse(_ data: Data, containerName: String) -> ParsingServiceResult? {
-        var toggleData = data
-
-        if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
-            let jsonContainer = json as? [String: Any],
-            let featureToggles = jsonContainer[containerName],
-            let featureTogglesData = try? JSONSerialization.data(withJSONObject: featureToggles) {
-                toggleData = featureTogglesData
-        }
-
-        return try? JSONDecoder().decode([FeatureToggle].self, from: toggleData)
-    }
+    private let isEnabledPrefix = "LocalFeatureToggleProvider.isEnabledPrefix"
 }
