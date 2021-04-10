666d1d42192fcd4b24a3e21fdebf241a5d0e0fd6
Vitaliy Yamshchikov <viik.ufa@gmail.com>
2021-04-10 13:29:22 +0500

    WIP: 196 - sync info

--- station/Classes/Persistence/Network/Impl/NetworkPersistenceImpl.swift
+++ station/Classes/Persistence/Network/Impl/NetworkPersistenceImpl.swift
@@ -1,6 +1,47 @@
 import Foundation
 
+enum NetworkSyncStatus: Int {
+    case none = 0
+    case syncing
+    case complete
+    case onError
+}
+
+enum NetworkSyncStatusKey: String {
+    case mac
+    case status
+}
+
 class NetworkPersistenceImpl: NetworkPersistence {
+
+    private let networkSyncStatusPrefix = "NetworkPersistence.syncState."
+
+    func setSyncStatus(_ status: NetworkSyncStatus, for macId: MACIdentifier) {
+        UserDefaults.standard.set(status.rawValue, forKey: networkSyncStatusPrefix + macId.mac)
+        DispatchQueue.main.async {
+            NotificationCenter
+                .default
+                .post(name: .NetworkSyncDidChangeStatus, object: nil, userInfo: [
+                    NetworkSyncStatusKey.mac: macId,
+                    NetworkSyncStatusKey.status: status
+                ])
+        }
+        switch status {
+        case .complete, .onError:
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1),
+                                          execute: { [weak self] in
+                self?.setSyncStatus(.none, for: macId)
+            })
+        default:
+            break
+        }
+    }
+
+    func getSyncStatus(for macId: MACIdentifier) -> NetworkSyncStatus {
+        let value = UserDefaults.standard.integer(forKey: networkSyncStatusPrefix + macId.mac)
+        return NetworkSyncStatus(rawValue: value) ?? .none
+    }
+
     @UserDefault("NetworkPersistence.lastSyncDate", defaultValue: nil)
     var lastSyncDate: Date? {
         didSet {
