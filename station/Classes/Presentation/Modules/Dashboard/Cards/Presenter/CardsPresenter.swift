e2d10cad473aaa5d3a7144c9f0158358e1e6e391
Vitaliy Yamshchikov <viik.ufa@gmail.com>
2021-04-19 23:48:59 +0500

    197 Network sensor is showing "Connected" status

--- station/Classes/Presentation/Modules/Dashboard/Cards/Presenter/CardsPresenter.swift
+++ station/Classes/Presentation/Modules/Dashboard/Cards/Presenter/CardsPresenter.swift
@@ -329,10 +329,9 @@ extension CardsPresenter {
                 viewModel.background.value = backgroundPersistence.background(for: macId)
                 viewModel.humidityOffset.value = calibrationService.humidityOffset(for: macId).0
                 viewModel.humidityOffsetDate.value = calibrationService.humidityOffset(for: macId).1
-                // viewModel.isConnected.value = background.isConnected(uuid: luid.value)
                 // viewModel.alertState.value = alertService.hasRegistrations(for: luid.value) ? .registered : .empty
                 viewModel.networkSyncStatus.value = networkPersistance.getSyncStatus(for: macId)
-                viewModel.isConnected.value = true
+                viewModel.isConnected.value = false
                 viewModel.alertState.value = .empty
             } else {
                 assertionFailure()
