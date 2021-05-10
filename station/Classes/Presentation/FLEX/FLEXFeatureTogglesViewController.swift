e8c41bb5da2370e1bca113fde6f76275d3e0005f
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-10 15:14:00 +0500

    register empty custom addition (feature toggles) in FLEX

---
+++ station/Classes/Presentation/FLEX/FLEXFeatureTogglesViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class FLEXFeatureTogglesViewController: UITableViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
