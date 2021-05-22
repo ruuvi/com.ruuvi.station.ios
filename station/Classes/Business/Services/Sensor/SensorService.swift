237fc351e2593200a0168044f6eabaec8c1c57fa
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-22 12:39:56 +0500

    #283 add setNextDefaultBackground to SensorService

--- station/Classes/Business/Services/Sensor/SensorService.swift
+++ station/Classes/Business/Services/Sensor/SensorService.swift
@@ -3,4 +3,5 @@ import Future
 
 protocol SensorService {
     func background(for identifier: Identifier) -> Future<UIImage, RUError>
+    func setNextDefaultBackground(for identifier: Identifier) -> Future<UIImage, RUError>
 }
