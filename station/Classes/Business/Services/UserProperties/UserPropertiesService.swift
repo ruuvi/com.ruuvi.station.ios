b4afb5bedda455f77ea9e76bff16a953a0c9eca6
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-22 11:10:24 +0500

    #283 remove header comments

--- station/Classes/Business/Services/UserProperties/UserPropertiesService.swift
+++ station/Classes/Business/Services/UserProperties/UserPropertiesService.swift
@@ -1,11 +1,3 @@
-//
-//  UserPropertiesService.swift
-//  station
-//
-//  Created by Vitaliy on 22.01.2021.
-//  Copyright © 2021 Ruuvi Innovations Oy. BSD-3-Clause.
-//
-
 import Foundation
 
 protocol UserPropertiesService {
