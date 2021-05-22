51e3dae9f89310779f436205f2e723a9ae527afa
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-22 12:39:56 +0500

    #283 add setNextDefaultBackground to SensorService

--- station/Classes/Business/Services/Sensor/Impl/SensorServiceImpl.swift
+++ station/Classes/Business/Services/Sensor/Impl/SensorServiceImpl.swift
@@ -13,4 +13,14 @@ final class SensorServiceImpl: SensorService {
         }
         return promise.future
     }
+
+    func setNextDefaultBackground(for identifier: Identifier) -> Future<UIImage, RUError> {
+        let promise = Promise<UIImage, RUError>()
+        if let image = backgroundPersistence.setNextDefaultBackground(for: identifier) {
+            promise.succeed(value: image)
+        } else {
+            promise.fail(error: .unexpected(.failedToFindOrGenerateBackgroundImage))
+        }
+        return promise.future
+    }
 }
