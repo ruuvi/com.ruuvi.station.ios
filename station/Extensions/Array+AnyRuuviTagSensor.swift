bdfe3cec1f19317a7cfc99738cddfc2cf7abae05
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-28 13:51:30 +0500

    remove settings from Reactor

---
+++ station/Extensions/Array+AnyRuuviTagSensor.swift
@@ -0,0 +1,9 @@
+//
+//  Array+AnyRuuviTagSensor.swift
+//  station
+//
+//  Created by Rinat Enikeev on 28.05.2021.
+//  Copyright © 2021 Ruuvi Innovations Oy. BSD-3-Clause.
+//
+
+import Foundation
