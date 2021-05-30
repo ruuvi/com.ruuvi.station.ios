b2ba3a4bebe65144ebbeb2605ad848b0a5d98368
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-05-30 11:55:24 +0500

    remove unused method

--- station/Classes/Networking/Ruuvi/UserApi/RuuviNetworkUserApi.swift
+++ station/Classes/Networking/Ruuvi/UserApi/RuuviNetworkUserApi.swift
@@ -6,7 +6,6 @@ import RuuviOntology
 
 /// https://docs.ruuvi.com/communication/ruuvi-network/backends/serverless/user-api
 protocol RuuviNetworkUserApi {
-    func shared(_ requestModel: UserApiSharedRequest) -> Future<UserApiSharedResponse, RUError>
     func update(_ requestModel: UserApiSensorUpdateRequest) -> Future<UserApiSensorUpdateResponse, RUError>
     func uploadImage(_ requestModel: UserApiSensorImageUploadRequest,
                      imageData: Data,
