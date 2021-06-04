ca904e6e57bec4c66bb770a61b0555c3ba9e4add
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-06-04 22:07:47 +0500

    #284 introduce RuuviRepository to handle offsets

---
+++ Packages/RuuviRepository/Sources/RuuviRepository/RuuviRepositoryError.swift
@@ -0,0 +1,9 @@
+//
+//  RuuviRepositoryError.swift
+//  RuuviRepository
+//
+//  Created by Rinat Enikeev on 04.06.2021.
+//  Copyright © 2021 Ruuvi Innovations Oy. BSD-3-Clause.
+//
+
+import Foundation
---
+++ station/Extensions/Errors/RuuviRepositoryError+LocalizedError.swift
@@ -0,0 +1,9 @@
+//
+//  RuuviRepositoryError+LocalizedError.swift
+//  station
+//
+//  Created by Rinat Enikeev on 04.06.2021.
+//  Copyright © 2021 Ruuvi Innovations Oy. BSD-3-Clause.
+//
+
+import Foundation
