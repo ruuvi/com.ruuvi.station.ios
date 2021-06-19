4aa89c793a6e8d32213ff935e154203e379cf987
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-06-19 21:21:22 +0500

    eliminate RxSwift from pod peoject

--- station/Classes/Reactor/VirtualTag/Impl/VirtualTagReactorImpl.swift
+++ station/Classes/Reactor/VirtualTag/Impl/VirtualTagReactorImpl.swift
@@ -5,15 +5,10 @@ import RuuviOntology
 import RuuviContext
 
 class VirtualTagReactorImpl: VirtualTagReactor {
-
     var realmContext: RealmContext!
     var realmPersistence: WebTagPersistence!
 
-    private lazy var entityRxSwift = VirtualTagSubjectRxSwift(realm: realmContext)
-    #if canImport(Combine)
-    @available(iOS 13, *)
     private lazy var entityCombine = VirtualTagSubjectCombine(realm: realmContext)
-    #endif
 
     func observe(_ block: @escaping (ReactorChange<AnyVirtualTagSensor>) -> Void) -> RUObservationToken {
         let realmOperation = realmPersistence.readAll()
@@ -22,55 +17,20 @@ class VirtualTagReactorImpl: VirtualTagReactor {
         }, failure: { error in
             block(.error(error))
         })
-
-        #if canImport(Combine)
-        if #available(iOS 13, *) {
-            let insert = entityCombine.insertSubject.sink { value in
-                block(.insert(value))
-            }
-            let update = entityCombine.updateSubject.sink { value in
-                block(.update(value))
-            }
-            let delete = entityCombine.deleteSubject.sink { value in
-                block(.delete(value))
-            }
-            return RUObservationToken {
-                insert.cancel()
-                update.cancel()
-                delete.cancel()
-            }
-        } else {
-            let insert = entityRxSwift.insertSubject.subscribe(onNext: { value in
-                block(.insert(value))
-            })
-            let update = entityRxSwift.updateSubject.subscribe(onNext: { value in
-                block(.update(value))
-            })
-            let delete = entityRxSwift.deleteSubject.subscribe(onNext: { value in
-                block(.delete(value))
-            })
-            return RUObservationToken {
-                insert.dispose()
-                update.dispose()
-                delete.dispose()
-            }
-        }
-        #else
-        let insert = entityRxSwift.insertSubject.subscribe(onNext: { value in
+        let insert = entityCombine.insertSubject.sink { value in
             block(.insert(value))
-        })
-        let update = entityRxSwift.updateSubject.subscribe(onNext: { value in
-            block(.update(value))
-        })
-        let delete = entityRxSwift.deleteSubject.subscribe(onNext: { value in
-            block(.delete(value))
-        })
-        return RUObservationToken {
-            insert.dispose()
-            update.dispose()
-            delete.dispose()
         }
-        #endif
+        let update = entityCombine.updateSubject.sink { value in
+            block(.update(value))
+        }
+        let delete = entityCombine.deleteSubject.sink { value in
+            block(.delete(value))
+        }
+        return RUObservationToken {
+            insert.cancel()
+            update.cancel()
+            delete.cancel()
+        }
     }
 
 }
