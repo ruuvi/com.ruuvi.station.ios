f5cf2c43afcf561871c56c3d9381397356c56f91
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-06-23 18:36:28 +0500

    move diff to RuuviCore

---
+++ Packages/RuuviCore/Sources/RuuviCoreDiff/Models/ReloadableSection.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct ReloadableSection<N: Equatable>: Equatable {
+    public var key: String
+    public var value: [ReloadableCell<N>]
+    public var index: Int
+
+    public static func == (lhs: ReloadableSection, rhs: ReloadableSection) -> Bool {
+        return lhs.key == rhs.key && lhs.value == rhs.value
+    }
+}
