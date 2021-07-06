d390dce55590a030b4566caf9da5d9874013974c
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-07-06 18:19:33 +0500

    #471 localized DFU

--- Packages/RuuviDFU/Sources/RuuviDFU/RuuviDFUError.swift
+++ Packages/RuuviDFU/Sources/RuuviDFU/RuuviDFUError.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct RuuviDfuError: Error {
     public static let invalidFirmwareFile = RuuviDfuError(description: "RuuviDfuError.invalidFirmwareFile")
-    public static let failedToConstructUUID = RuuviDfuError(description: "RuuviDfuError.failedToConstructUUID") // TODO: @rinat localize
+    public static let failedToConstructUUID = RuuviDfuError(description: "RuuviDfuError.failedToConstructUUID")
     public let description: String
     public init(description: String) {
         self.description = description
