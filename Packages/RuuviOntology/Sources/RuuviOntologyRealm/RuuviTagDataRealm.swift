d2544b310360558ae4a4e9b7bb6df934f3867abe
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-07-09 15:00:53 +0500

    #498 dont respect temperature when saving relative humidity

--- Packages/RuuviOntology/Sources/RuuviOntologyRealm/RuuviTagDataRealm.swift
+++ Packages/RuuviOntology/Sources/RuuviOntologyRealm/RuuviTagDataRealm.swift
@@ -91,10 +91,7 @@ public final class RuuviTagDataRealm: Object {
         self.sourceString = record.source.rawValue
         self.rssi.value = record.rssi
         self.celsius.value = record.temperature?.converted(to: .celsius).value
-        if let temperature = record.temperature {
-            let humidity = record.humidity?.converted(to: .relative(temperature: temperature))
-            self.humidity.value = humidity?.value
-        }
+        self.humidity.value = record.humidity?.value
         self.pressure.value = record.pressure?.converted(to: .hectopascals).value
         self.accelerationX.value = record.acceleration?.x.value
         self.accelerationY.value = record.acceleration?.y.value
