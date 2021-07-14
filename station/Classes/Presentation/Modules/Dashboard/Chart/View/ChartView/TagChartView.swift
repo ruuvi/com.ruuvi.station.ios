d7b407e371ebc978deaaf84e31cf48876dbd1be9
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-07-14 16:31:37 +0500

    #137 the grid is fixed, left and right labels are shown, right axis is shown

--- station/Classes/Presentation/Modules/Dashboard/Chart/View/ChartView/TagChartView.swift
+++ station/Classes/Presentation/Modules/Dashboard/Chart/View/ChartView/TagChartView.swift
@@ -123,6 +123,7 @@ class TagChartView: LineChartView {
         highlightPerDragEnabled = false
         backgroundColor = .clear
         legend.enabled = false
+
         xAxis.labelPosition = .bottom
         xAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
         xAxis.labelTextColor = UIColor.white
@@ -132,13 +133,23 @@ class TagChartView: LineChartView {
         xAxis.granularity = 59.9
         xAxis.valueFormatter = DateValueFormatter(with: settings?.language.locale ?? Locale.current)
         xAxis.granularityEnabled = true
+        xAxis.setLabelCount(9, force: true)
+
         leftAxis.labelPosition = .outsideChart
         leftAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
         leftAxis.drawGridLinesEnabled = true
         leftAxis.labelTextColor = UIColor.white
         leftAxis.minWidth = 30.0
         leftAxis.maxWidth = 30.0
-        rightAxis.enabled = false
+
+        rightAxis.enabled = true
+        rightAxis.minWidth = 30.0
+        rightAxis.maxWidth = 30.0
+        rightAxis.labelPosition = .outsideChart
+        rightAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
+        rightAxis.drawGridLinesEnabled = true
+        rightAxis.labelTextColor = UIColor.white
+
         legend.form = .line
         noDataTextColor = UIColor.white
         noDataText = "TagCharts.NoChartData.text".localized()
