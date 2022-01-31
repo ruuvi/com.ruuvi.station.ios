252549c1bcc90b1e0a6a91e6ca628ba334619743
priyonto <priyontoo@gmail.com>
2022-01-31 09:31:45 +0100

    fix: Show localized error for sensor claim #604
    Breaking changes!
    
    This is a fat commit with changes in several files but all related to one single issue. This fix affected both service and presentation layer, hence the changes were required.

--- station/Classes/Presentation/Modules/TagSettings/Submodules/Owner/View/OwnerViewController.swift
+++ station/Classes/Presentation/Modules/TagSettings/Submodules/Owner/View/OwnerViewController.swift
@@ -17,6 +17,22 @@ final class OwnerViewController: UIViewController {
 }
 
 extension OwnerViewController: OwnerViewInput {
+    func showSensorAlreadyClaimedError(error: String, email: String?) {
+        var message: String = ""
+        if let email = email {
+            // If there's email in the error
+            message = String.localizedStringWithFormat(error.localized(),
+                                                       email)
+        } else {
+            // if there's no email address in the error
+            message = "UserApiError.ER_SENSOR_ALREADY_CLAIMED_NO_EMAIL".localized()
+        }
+        let alertVC = UIAlertController(title: "ErrorPresenterAlert.Error".localized(),
+                                        message: message,
+                                        preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK".localized(), style: .cancel, handler: nil))
+        present(alertVC, animated: true)
+    }
     func localize() {
         title = "Owner.title".localized()
         claimOwnershipDescriptionLabel.text = "Owner.Claim.description".localized()
