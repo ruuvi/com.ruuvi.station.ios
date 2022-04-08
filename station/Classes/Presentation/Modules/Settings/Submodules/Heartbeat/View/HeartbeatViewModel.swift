df0d6c9f8d8aebc474147446a1ea091c55cac99d
priyonto <priyontoo@gmail.com>
2022-04-08 15:20:03 +0200

    feature: Remove RSSI settings #402

--- station/Classes/Presentation/Modules/Settings/Submodules/Heartbeat/View/HeartbeatViewModel.swift
+++ station/Classes/Presentation/Modules/Settings/Submodules/Heartbeat/View/HeartbeatViewModel.swift
@@ -3,14 +3,8 @@ import Foundation
 class HeartbeatViewModel {
     var saveHeartbeats = Observable<Bool?>()
     var saveHeartbeatsInterval = Observable<Int?>(1)
-    var readRSSI = Observable<Bool?>()
-    var readRSSIInterval = Observable<Int?>(5)
 
     var saveHeartbeatsTitle: String {
         return "Heartbeat.SaveHeartbeats.title".localized()
     }
-    var readRSSITitle: String {
-        return "Heartbeat.readRSSITitle.title".localized()
-    }
-
 }
