417fa867f4554a8880cdc26c43c53fb7bd986e0e
Priyonto M Rahman <priyontoo@gmail.com>
2023-06-28 22:21:01 -0400

    task: Implement unclaim sensor #1462

--- station/Classes/Presentation/Modules/TagSettings/Submodules/Owner/View/OwnerViewInput.swift
+++ station/Classes/Presentation/Modules/TagSettings/Submodules/Owner/View/OwnerViewInput.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 protocol OwnerViewInput: ViewInput {
+    var mode: OwnershipMode { get set }
     func showSensorAlreadyClaimedDialog()
     func showFirmwareUpdateDialog()
     func showFirmwareDismissConfirmationUpdateDialog()
