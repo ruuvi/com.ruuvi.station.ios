c3eaba99975c2969518423cce9ff272dbfbc92c4
Rinat Enikeev <rinat.enikeev@gmail.com>
2023-11-25 11:12:10 +0200

    Xcodegen tool installation if needed Added script to install xcodegen, so `make` works even without `brew install xcodegen`

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
+.PHONY: installed_xcodegen
+
 # generates xcodeproj for frameworks build configuration
-xcodeproj_with_frameworks:
-	xcodegen -s project_frameworks.yml
+xcodeproj_with_frameworks: installed_xcodegen
+	.tools/xcodegen/bin/xcodegen -s project_frameworks.yml
+
+# install xcodegen
+installed_xcodegen: .tools/xcodegen/bin/xcodegen
+
+# install xcodegen if not installed
+.tools/xcodegen/bin/xcodegen: scripts/install/install_xcodegen.sh
+	scripts/install/install_xcodegen.sh
+	touch $@
 
 # generates xcodeproj for swift package manager build configuration
-xcodeproj_with_spm:
-	xcodegen -s project_spm.yml
+xcodeproj_with_spm: installed_xcodegen
+	.tools/xcodegen/bin/xcodegen -s project_spm.yml
 
 # builds station target with frameworks build configuration for iOS
-build_with_frameworks:
+build_with_frameworks: 
 	d=$$(date +%s)\
 	; xcodebuild -project frameworks.xcodeproj -scheme station -configuration Release -sdk iphoneos17.0 build\
     && echo "Build took $$(($$(date +%s)-d)) seconds"
---
+++ scripts/install/install_xcodegen.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# immediately exit if any command has a non-zero exit status
+set -e
+
+OUTPUT_DIR=".tools/xcodegen"
+
+EXPECTED_XCODEGEN_HASH="2566750f11478c88c2c114a120d7d7fc"
+
+# check if xcodegen is already installed by hashing all files in the output directory
+if [ -d "$OUTPUT_DIR" ] && [ "$(find "$OUTPUT_DIR" -type f -exec md5 {} \; | md5)" = $EXPECTED_XCODEGEN_HASH ]; then
+  echo "xcodegen is already installed"
+  exit 0
+fi
+
+# remove the output directory if it exists
+if [ -d "$OUTPUT_DIR" ]; then
+  rm -rf "$OUTPUT_DIR"
+fi
+
+# create the output directory
+mkdir -p "$OUTPUT_DIR"
+
+# download the latest release of xcodegen
+curl -L "https://github.com/yonaskolb/XcodeGen/releases/download/2.38.0/xcodegen.zip" -o ".tools/xcodegen.zip"
+
+# unzip the downloaded file
+unzip ".tools/xcodegen.zip" -d ".tools"
+
+# remove the downloaded file
+rm ".tools/xcodegen.zip"
+
+# echo the hash of downloaded xcodegen
+echo "$(md5 .tools/xcodegen)"
+
+# if the hash of downloaded xcodegen is not the expected value, exit with an error
+INSTALLED_XCODEGEN_HASH="$(find "$OUTPUT_DIR" -type f -exec md5 {} \; | md5)"
+if [ "$INSTALLED_XCODEGEN_HASH" != "$EXPECTED_XCODEGEN_HASH" ]; then
+  echo "xcodegen failed to install"
+  echo "Expected hash: $EXPECTED_XCODEGEN_HASH"
+  echo "Actual hash: $INSTALLED_XCODEGEN_HASH"
+  exit 1
+else
+    echo "No xcodegen install needed" 
+fi 
