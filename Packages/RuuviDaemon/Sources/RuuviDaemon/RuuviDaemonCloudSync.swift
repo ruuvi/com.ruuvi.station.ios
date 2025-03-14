e5fa48c07158d75828fe9d98f89308626afb32fd
Priyonto M Rahman <priyontoo@gmail.com>
2025-03-14 19:12:20 +0100

    fix: Move cloud sync daemon start call to applicationDidBecomeActive
    This prevents cloud sync daemon to wake up when things like background scanning may wake up app for processing background operations.

--- Packages/RuuviDaemon/Sources/RuuviDaemon/RuuviDaemonCloudSync.swift
+++ Packages/RuuviDaemon/Sources/RuuviDaemon/RuuviDaemonCloudSync.swift
@@ -3,6 +3,7 @@ import Foundation
 public protocol RuuviDaemonCloudSync {
     func start()
     func stop()
+    func isRunning() -> Bool
     func refreshImmediately()
     func refreshLatestRecord()
 }
