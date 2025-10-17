a4c1c62b70404ef330d03c9a81d90098235c2d19
Priyonto M Rahman <priyontoo@gmail.com>
2025-10-17 20:55:03 +0200

    refactor: Enforce persistent layer to match last 3 bytes of Mac
    Earlier some of the functions were skipped. Now, all functions are enforced to check last 3 bytes for MacID.

--- Packages/RuuviPersistence/Sources/RuuviPersistenceSQLite/RuuviPersistenceSQLite.swift
+++ Packages/RuuviPersistence/Sources/RuuviPersistenceSQLite/RuuviPersistenceSQLite.swift
@@ -32,26 +32,27 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
     public func create(_ ruuviTag: RuuviTagSensor) -> Future<Bool, RuuviPersistenceError> {
         let promise = Promise<Bool, RuuviPersistenceError>()
         assert(ruuviTag.macId != nil)
-        let entity = Entity(
-            id: ruuviTag.id,
-            macId: ruuviTag.macId,
-            luid: ruuviTag.luid,
-            serviceUUID: ruuviTag.serviceUUID,
-            name: ruuviTag.name,
-            version: ruuviTag.version,
-            firmwareVersion: ruuviTag.firmwareVersion,
-            isConnectable: ruuviTag.isConnectable,
-            isClaimed: ruuviTag.isClaimed,
-            isOwner: ruuviTag.isOwner,
-            owner: ruuviTag.owner,
-            ownersPlan: ruuviTag.ownersPlan,
-            isCloudSensor: ruuviTag.isCloudSensor,
-            canShare: ruuviTag.canShare,
-            sharedTo: ruuviTag.sharedTo,
-            maxHistoryDays: ruuviTag.maxHistoryDays
-        )
         do {
             try database.dbPool.write { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
+                let entity = Entity(
+                    id: normalizedTag.id,
+                    macId: normalizedTag.macId,
+                    luid: normalizedTag.luid,
+                    serviceUUID: normalizedTag.serviceUUID,
+                    name: normalizedTag.name,
+                    version: normalizedTag.version,
+                    firmwareVersion: normalizedTag.firmwareVersion,
+                    isConnectable: normalizedTag.isConnectable,
+                    isClaimed: normalizedTag.isClaimed,
+                    isOwner: normalizedTag.isOwner,
+                    owner: normalizedTag.owner,
+                    ownersPlan: normalizedTag.ownersPlan,
+                    isCloudSensor: normalizedTag.isCloudSensor,
+                    canShare: normalizedTag.canShare,
+                    sharedTo: normalizedTag.sharedTo,
+                    maxHistoryDays: normalizedTag.maxHistoryDays
+                )
                 try entity.insert(db)
             }
             promise.succeed(value: true)
@@ -66,7 +67,9 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         assert(record.macId != nil)
         do {
             try database.dbPool.write { db in
-                try record.sqlite.insert(db)
+                let normalizedRecord = try normalizedRecord(record, db: db)
+                assert(normalizedRecord.macId != nil)
+                try normalizedRecord.sqlite.insert(db)
             }
             promise.succeed(value: true)
         } catch {
@@ -80,7 +83,9 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         assert(record.macId != nil)
         do {
             try database.dbPool.write { db in
-                try record.latest.insert(db)
+                let normalizedRecord = try normalizedRecord(record, db: db)
+                assert(normalizedRecord.macId != nil)
+                try normalizedRecord.latest.insert(db)
             }
             promise.succeed(value: true)
         } catch {
@@ -94,7 +99,9 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         assert(record.macId != nil)
         do {
             try database.dbPool.write { db in
-                try record.latest.update(db)
+                let normalizedRecord = try normalizedRecord(record, db: db)
+                assert(normalizedRecord.macId != nil)
+                try normalizedRecord.latest.update(db)
             }
             promise.succeed(value: true)
         } catch {
@@ -109,7 +116,9 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
             try database.dbPool.write { db in
                 for record in records {
                     assert(record.macId != nil)
-                    try record.sqlite.insert(db)
+                    let normalizedRecord = try normalizedRecord(record, db: db)
+                    assert(normalizedRecord.macId != nil)
+                    try normalizedRecord.sqlite.insert(db)
                 }
             }
             promise.succeed(value: true)
@@ -415,27 +424,28 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
     public func update(_ ruuviTag: RuuviTagSensor) -> Future<Bool, RuuviPersistenceError> {
         let promise = Promise<Bool, RuuviPersistenceError>()
         assert(ruuviTag.macId != nil)
-        let entity = Entity(
-            id: ruuviTag.id,
-            macId: ruuviTag.macId,
-            luid: ruuviTag.luid,
-            serviceUUID: ruuviTag.serviceUUID,
-            name: ruuviTag.name,
-            version: ruuviTag.version,
-            firmwareVersion: ruuviTag.firmwareVersion,
-            isConnectable: ruuviTag.isConnectable,
-            isClaimed: ruuviTag.isClaimed,
-            isOwner: ruuviTag.isOwner,
-            owner: ruuviTag.owner,
-            ownersPlan: ruuviTag.ownersPlan,
-            isCloudSensor: ruuviTag.isCloudSensor,
-            canShare: ruuviTag.canShare,
-            sharedTo: ruuviTag.sharedTo,
-            maxHistoryDays: ruuviTag.maxHistoryDays
-        )
 
         do {
             try database.dbPool.write { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
+                let entity = Entity(
+                    id: normalizedTag.id,
+                    macId: normalizedTag.macId,
+                    luid: normalizedTag.luid,
+                    serviceUUID: normalizedTag.serviceUUID,
+                    name: normalizedTag.name,
+                    version: normalizedTag.version,
+                    firmwareVersion: normalizedTag.firmwareVersion,
+                    isConnectable: normalizedTag.isConnectable,
+                    isClaimed: normalizedTag.isClaimed,
+                    isOwner: normalizedTag.isOwner,
+                    owner: normalizedTag.owner,
+                    ownersPlan: normalizedTag.ownersPlan,
+                    isCloudSensor: normalizedTag.isCloudSensor,
+                    canShare: normalizedTag.canShare,
+                    sharedTo: normalizedTag.sharedTo,
+                    maxHistoryDays: normalizedTag.maxHistoryDays
+                )
                 try entity.update(db)
             }
             promise.succeed(value: true)
@@ -448,30 +458,30 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
     public func delete(_ ruuviTag: RuuviTagSensor) -> Future<Bool, RuuviPersistenceError> {
         let promise = Promise<Bool, RuuviPersistenceError>()
         assert(ruuviTag.macId != nil)
-        let entity = Entity(
-            id: ruuviTag.id,
-            macId: ruuviTag.macId,
-            luid: ruuviTag.luid,
-            serviceUUID: ruuviTag.serviceUUID,
-            name: ruuviTag.name,
-            version: ruuviTag.version,
-            firmwareVersion: ruuviTag.firmwareVersion,
-            isConnectable: ruuviTag.isConnectable,
-            isClaimed: ruuviTag.isClaimed,
-            isOwner: ruuviTag.isOwner,
-            owner: ruuviTag.owner,
-            ownersPlan: ruuviTag.ownersPlan,
-            isCloudSensor: ruuviTag.isCloudSensor,
-            canShare: ruuviTag.canShare,
-            sharedTo: ruuviTag.sharedTo,
-            maxHistoryDays: ruuviTag.maxHistoryDays
-        )
         do {
-            var success = false
             try database.dbPool.write { db in
-                success = try entity.delete(db)
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
+                let entity = Entity(
+                    id: normalizedTag.id,
+                    macId: normalizedTag.macId,
+                    luid: normalizedTag.luid,
+                    serviceUUID: normalizedTag.serviceUUID,
+                    name: normalizedTag.name,
+                    version: normalizedTag.version,
+                    firmwareVersion: normalizedTag.firmwareVersion,
+                    isConnectable: normalizedTag.isConnectable,
+                    isClaimed: normalizedTag.isClaimed,
+                    isOwner: normalizedTag.isOwner,
+                    owner: normalizedTag.owner,
+                    ownersPlan: normalizedTag.ownersPlan,
+                    isCloudSensor: normalizedTag.isCloudSensor,
+                    canShare: normalizedTag.canShare,
+                    sharedTo: normalizedTag.sharedTo,
+                    maxHistoryDays: normalizedTag.maxHistoryDays
+                )
+                let success = try entity.delete(db)
+                promise.succeed(value: success)
             }
-            promise.succeed(value: success)
         } catch {
             promise.fail(error: .grdb(error))
         }
@@ -549,13 +559,14 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         do {
             var sqliteSensorSettings: Settings?
             try database.dbPool.read { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
                 let request = Settings.filter(
                     (
-                        ruuviTag.luid?.value != nil && Settings.luidColumn == ruuviTag.luid?.value
+                        normalizedTag.luid?.value != nil && Settings.luidColumn == normalizedTag.luid?.value
                     )
                     || (
-                        ruuviTag.macId?.value != nil && Settings.macIdColumn.like(
-                            "%\(ruuviTag.macId!.value.lastThreeBytes)"
+                        normalizedTag.macId?.value != nil && Settings.macIdColumn.like(
+                            "%\(normalizedTag.macId!.value.lastThreeBytes)"
                         )
                     )
                 )
@@ -593,22 +604,23 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         let promise = Promise<SensorSettings, RuuviPersistenceError>()
         assert(ruuviTag.macId != nil)
         do {
-            var isAddNewRecord = true
-            var sqliteSensorSettings = Settings(
-                luid: ruuviTag.luid,
-                macId: ruuviTag.macId,
-                temperatureOffset: nil,
-                humidityOffset: nil,
-                pressureOffset: nil
-            )
-            try database.dbPool.read { db in
+            let settings: Settings = try database.dbPool.write { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
+                var isAddNewRecord = true
+                var sqliteSensorSettings = Settings(
+                    luid: normalizedTag.luid,
+                    macId: normalizedTag.macId,
+                    temperatureOffset: nil,
+                    humidityOffset: nil,
+                    pressureOffset: nil
+                )
                 let request = Settings.filter(
                     (
-                        ruuviTag.luid?.value != nil && Settings.luidColumn == ruuviTag.luid?.value
+                        normalizedTag.luid?.value != nil && Settings.luidColumn == normalizedTag.luid?.value
                     )
                     || (
-                        ruuviTag.macId?.value != nil && Settings.macIdColumn.like(
-                            "%\(ruuviTag.macId!.value.lastThreeBytes)"
+                        normalizedTag.macId?.value != nil && Settings.macIdColumn.like(
+                            "%\(normalizedTag.macId!.value.lastThreeBytes)"
                         )
                     )
                 )
@@ -616,29 +628,31 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
                     sqliteSensorSettings = existingSettings
                     isAddNewRecord = false
                 }
-            }
-            switch type {
-            case .humidity:
-                sqliteSensorSettings.humidityOffset = value
-            case .pressure:
-                sqliteSensorSettings.pressureOffset = value
-            default:
-                sqliteSensorSettings.temperatureOffset = value
-            }
-            try database.dbPool.write { db in
+
+                switch type {
+                case .humidity:
+                    sqliteSensorSettings.humidityOffset = value
+                case .pressure:
+                    sqliteSensorSettings.pressureOffset = value
+                default:
+                    sqliteSensorSettings.temperatureOffset = value
+                }
+
                 if isAddNewRecord {
                     try sqliteSensorSettings.insert(db)
                 } else {
                     try sqliteSensorSettings.update(db)
                 }
-            }
-            if let sqliteSensorRecord = record {
-                try database.dbPool.write { db in
-                    try sqliteSensorRecord
-                        .sqlite.insert(db)
+
+                if let sqliteSensorRecord = record {
+                    let normalizedRecord = try normalizedRecord(sqliteSensorRecord, db: db)
+                    try normalizedRecord.sqlite.insert(db)
                 }
+
+                return sqliteSensorSettings
             }
-            promise.succeed(value: sqliteSensorSettings)
+
+            promise.succeed(value: settings)
         } catch let e {
             promise.fail(error: .grdb(e))
         }
@@ -652,13 +666,14 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         do {
             var success = false
             try database.dbPool.write { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
                 let request = Settings.filter(
                     (
-                        ruuviTag.luid?.value != nil && Settings.luidColumn == ruuviTag.luid?.value
+                        normalizedTag.luid?.value != nil && Settings.luidColumn == normalizedTag.luid?.value
                     )
                     || (
-                        ruuviTag.macId?.value != nil && Settings.macIdColumn.like(
-                            "%\(ruuviTag.macId!.value.lastThreeBytes)"
+                        normalizedTag.macId?.value != nil && Settings.macIdColumn.like(
+                            "%\(normalizedTag.macId!.value.lastThreeBytes)"
                         )
                     )
                 )
@@ -834,10 +849,12 @@ public class RuuviPersistenceSQLite: RuuviPersistence, DatabaseService {
         let promise = Promise<CloudSensorSubscription?, RuuviPersistenceError>()
         do {
             var sqliteSensorSettings: CloudSensorSubscription?
-            let request = SensorSubscription.filter(
-                ruuviTag.macId?.value != nil && SensorSubscription.macIdColumn == ruuviTag.macId?.value
-            )
             try database.dbPool.read { db in
+                let normalizedTag = try normalizedSensor(ruuviTag, db: db)
+                let request = SensorSubscription.filter(
+                    normalizedTag.macId?.value != nil
+                    && SensorSubscription.macIdColumn == normalizedTag.macId?.value
+                )
                 sqliteSensorSettings = try request.fetchOne(db)
             }
             promise.succeed(value: sqliteSensorSettings)
@@ -898,9 +915,117 @@ extension RuuviPersistenceSQLite {
         }
         return promise.future
     }
+
+    private func normalizedSensor(
+        _ sensor: RuuviTagSensor,
+        db: Database
+    ) throws -> RuuviTagSensor {
+        guard
+            let normalizedMac = try normalizedMacId(
+                macId: sensor.macId,
+                luid: sensor.luid,
+                db: db
+            ),
+            let currentMac = sensor.macId
+        else {
+            return sensor
+        }
+
+        if currentMac.value == normalizedMac.value {
+            return sensor
+        }
+
+        if currentMac.value.isLast3BytesEqual(to: normalizedMac.value) {
+            return sensor.with(macId: normalizedMac)
+        }
+
+        return sensor
+    }
+
+    private func normalizedRecord(
+        _ record: RuuviTagSensorRecord,
+        db: Database
+    ) throws -> RuuviTagSensorRecord {
+        guard
+            let normalizedMac = try normalizedMacId(
+                macId: record.macId,
+                luid: record.luid,
+                db: db
+            ),
+            let currentMac = record.macId
+        else {
+            return record
+        }
+
+        if currentMac.value == normalizedMac.value {
+            return record
+        }
+
+        if currentMac.value.isLast3BytesEqual(to: normalizedMac.value) {
+            return record.with(macId: normalizedMac)
+        }
+
+        return record
+    }
+
+    private func normalizedMacId(
+        macId: MACIdentifier?,
+        luid: LocalIdentifier?,
+        db: Database
+    ) throws -> MACIdentifier? {
+        guard let macId else {
+            return nil
+        }
+
+        if let luid,
+           let existing = try Entity
+            .filter(Entity.luidColumn == luid.value)
+            .fetchOne(db)?.macId {
+            return existing
+        }
+
+        if let existingExact = try Entity
+            .filter(Entity.macColumn == macId.value)
+            .fetchOne(db)?.macId {
+            return existingExact
+        }
+
+        let candidates = try fetchCandidateMacsMatchingSuffix(of: macId, db: db)
+        if candidates.count == 1 {
+            return candidates[0]
+        }
+
+        return macId
+    }
+
+    private func fetchCandidateMacsMatchingSuffix(
+        of macId: MACIdentifier,
+        db: Database
+    ) throws -> [MACIdentifier] {
+        var matches = [MACIdentifier]()
+        let suffix = macId.value.lastThreeBytes
+
+        if !suffix.isEmpty {
+            let likeMatches = try Entity
+                .filter(Entity.macColumn.like("%\(suffix)"))
+                .fetchAll(db)
+                .compactMap(\.macId)
+                .filter { $0.value.isLast3BytesEqual(to: macId.value) }
+            matches.append(contentsOf: likeMatches)
+        }
+
+        if matches.isEmpty {
+            let allMatches = try Entity
+                .fetchAll(db)
+                .compactMap(\.macId)
+                .filter { $0.value.isLast3BytesEqual(to: macId.value) }
+            matches.append(contentsOf: allMatches)
+        }
+
+        return matches
+    }
 }
 
-// Add this extension at the top of the file, after imports
 private extension String {
     /// Returns the last 3 bytes of a MAC address (e.g., "DD:EE:FF" from "AA:BB:CC:DD:EE:FF")
     var lastThreeBytes: String {
